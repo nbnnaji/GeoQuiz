1b2a835a620e31f01933b2e2cf447d1a94c52dd6
Nkechi B. Nnaji <nkechi.nnaji@udc.edu>
2017-07-14 02:05:53 -0500

    Initial commit

---
+++ app/src/main/java/com/mycodingskills/nnaji_nkechi/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package com.mycodingskills.nnaji_nkechi.geoquiz;
+
+/**
+ * Created by Nnaji-MacPro1 on 7/14/17.
+ */
+
+/**
+ * MODEL OBJECT
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue){
+
+    }
+}
